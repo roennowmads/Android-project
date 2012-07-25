1d3fa9b95c7e09dd350f68b9a67afd1abc4782fa
Mads <mads@Laptop-Linux.(none)>
2012-07-25 22:44:40 +0200

    Added awesome stufffff

--- Test002/src/com/example/test002/MainActivity.java
+++ Test002/src/com/example/test002/MainActivity.java
@@ -4,35 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-
-import com.example.test002.R.layout;
-
 import android.net.http.AndroidHttpClient;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.content.Intent;
 import android.content.res.Configuration;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.google.ads.AdView;
 import com.google.ads.AdSize;
 import com.google.ads.AdRequest;
 
 public class MainActivity extends Activity {
 	
-
 	private String ipResult = null;
 	
     @Override
@@ -75,6 +70,49 @@ public class MainActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
+        
+        final Activity thisActivity = this;
+        
+        menu.findItem(R.id.refresh).setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+			@Override
+			public boolean onMenuItemClick(MenuItem arg0) {
+				ProgressBar progress = (ProgressBar) findViewById(R.id.progressBar1);
+				progress.setVisibility(View.VISIBLE);
+				TextView tv = (TextView) findViewById(R.id.textView1);
+				tv.setVisibility(View.GONE);
+				
+				Task t = new Task();
+				t.execute("http://ifconfig.me/ip");
+				return true;
+			}
+    		
+    	});
+        menu.findItem(R.id.copy).setOnMenuItemClickListener(new OnMenuItemClickListener() {
+
+			@SuppressWarnings("deprecation")
+			@SuppressLint("NewApi")
+			@Override
+			public boolean onMenuItemClick(MenuItem item) {
+				String copiedIP = ipResult;
+				
+				int sdk = android.os.Build.VERSION.SDK_INT;
+				if(sdk < android.os.Build.VERSION_CODES.HONEYCOMB) {
+					android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+				    clipboard.setText(copiedIP);
+				} else {
+				    android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+				    android.content.ClipData clip = android.content.ClipData.newPlainText("IP address", copiedIP);
+				    clipboard.setPrimaryClip(clip);
+				}
+				
+				Toast toast = Toast.makeText(thisActivity, "IP address copied to clipboard", 2);
+				toast.show();
+				
+				return false;
+			}
+        	
+        });
         return true;
     }
     
@@ -99,7 +137,6 @@ public class MainActivity extends Activity {
 				
 			} catch (IOException e) {
 				r = "Error";
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
